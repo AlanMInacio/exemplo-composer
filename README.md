30447c47ce110a83d59d07c01f24ea0e18454e32
Alan Moura <alan.moura526@gmail.com>
2020-12-03 17:12:40 -0300

    first commit.sample php and composer lib slugify

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exemplo-composer
 exemplo composer 
+
+## Exemplo de composer com slugify
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require 'vendor/autoload.php';
+
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+echo $slugify->slugify('Hello World!'); // hello-world
+
+?>
